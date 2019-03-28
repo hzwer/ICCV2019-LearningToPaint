81c58db430d5b3f3511dea97db06e9df07e502c7
hzwer <598460606@163.com>
2019-03-28 12:55:59 +0800

    Update model.py

--- baseline/Renderer/model.py
+++ baseline/Renderer/model.py
@@ -4,9 +4,9 @@ import torch.nn.functional as F
 import torch.nn.utils.weight_norm as weightNorm
 
 class FCN(nn.Module):
-    def __init__(self, size=512):
+    def __init__(self):
         super().__init__()
-        self.fc1 = (nn.Linear(10, size))
+        self.fc1 = (nn.Linear(10, 512))
         self.fc2 = (nn.Linear(512, 1024))
         self.fc3 = (nn.Linear(1024, 2048))
         self.fc4 = (nn.Linear(2048, 4096))
