22c3c9e6070704a540695e0ea64bd8f38c300c48
hzwer <598460606@163.com>
2019-04-11 09:40:22 +0800

    Add draw width

--- baseline/Renderer/stroke_gen.py
+++ baseline/Renderer/stroke_gen.py
@@ -1,25 +1,22 @@
 import cv2
 import numpy as np
 
-canvas_width = 256
-output_width = 128
+def normal(x, width):
+    return (int)(x * (width - 1) + 0.5)
 
-def normal(x):
-    return (int)(x * (canvas_width - 1) + 0.5)
-
-def draw(f):
+def draw(f, width=128):
     x0, y0, x1, y1, x2, y2, z0, z2, w0, w2 = f
     x1 = x0 + (x2 - x0) * x1
     y1 = y0 + (y2 - y0) * y1
-    x0 = normal(x0)
-    x1 = normal(x1)
-    x2 = normal(x2)
-    y0 = normal(y0)
-    y1 = normal(y1)
-    y2 = normal(y2)
-    z0 = (int)(1 + z0 * 64)
-    z2 = (int)(1 + z2 * 64)
-    canvas = np.zeros([canvas_width, canvas_width]).astype('float32')
+    x0 = normal(x0, width * 2)
+    x1 = normal(x1, width * 2)
+    x2 = normal(x2, width * 2)
+    y0 = normal(y0, width * 2)
+    y1 = normal(y1, width * 2)
+    y2 = normal(y2, width * 2)
+    z0 = (int)(1 + z0 * width // 2)
+    z2 = (int)(1 + z2 * width // 2)
+    canvas = np.zeros([width * 2, width * 2]).astype('float32')
     tmp = 1. / 100
     for i in range(100):
         t = i * tmp
@@ -28,4 +25,4 @@ def draw(f):
         z = (int)((1-t) * z0 + t * z2)
         w = (1-t) * w0 + t * w2
         cv2.circle(canvas, (y, x), z, w, -1)
-    return 1 - cv2.resize(canvas, dsize=(output_width, output_width))
+    return 1 - cv2.resize(canvas, dsize=(width, width))
