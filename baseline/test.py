eafbccce9615ff45baf7de87cc9627ea01940af6
hzwer <598460606@163.com>
2019-04-22 01:23:07 +0800

    smooth the edge of every patch

--- baseline/test.py
+++ baseline/test.py
@@ -20,12 +20,13 @@ parser.add_argument('--renderer', default='./renderer.pkl', type=str, help='rend
 parser.add_argument('--img', default='image/test.png', type=str, help='test image')
 parser.add_argument('--imgid', default=0, type=int, help='set begin number for generated image')
 parser.add_argument('--divide', default=4, type=int, help='divide the target image to get better resolution')
+parser.add_argument('--smooth', default=True, type=bool, help='smooth the edge of every patch')
 args = parser.parse_args()
 
 canvas_cnt = args.divide * args.divide
 T = torch.ones([1, 1, width, width], dtype=torch.float32).to(device)
 img = cv2.imread(args.img, cv2.IMREAD_COLOR)
-origin_shape = img.shape[:-1]
+origin_shape = (img.shape[1], img.shape[0])
 
 coord = torch.zeros([1, 2, width, width])
 for i in range(width):
@@ -66,16 +67,41 @@ def large2small(x):
     x = x.reshape(canvas_cnt, width, width, 3)
     return x
 
+def smooth(img):
+    def smooth_pix(img, tx, ty):
+        if tx == args.divide * width - 1 or ty == args.divide * width - 1 or tx == 0 or ty == 0: 
+            return img
+        img[tx, ty] = (img[tx, ty] + img[tx + 1, ty] + img[tx, ty + 1] + img[tx - 1, ty] + img[tx, ty - 1] + img[tx + 1, ty - 1] + img[tx - 1, ty + 1] + img[tx - 1, ty - 1] + img[tx + 1, ty + 1]) / 9
+        return img
+
+    for p in range(args.divide):
+        for q in range(args.divide):
+            x = p * width
+            y = q * width
+#            if p == args.divide - 1 and q == args.divide - 1:
+#                continue
+            for k in range(width):
+                img = smooth_pix(img, x + k, y + width - 1)
+                if q != args.divide - 1:
+                    img = smooth_pix(img, x + k, y + width)
+            for k in range(width):
+                img = smooth_pix(img, x + width - 1, y + k)
+                if p != args.divide - 1:
+                    img = smooth_pix(img, x + width, y + k)
+    return img
+
 def save_img(res, imgid, divide=False):
     output = res.detach().cpu().numpy() # d * d, 3, width, width    
     output = np.transpose(output, (0, 2, 3, 1))
     if divide:
         output = small2large(output)
+        if args.smooth:
+            output = smooth(output)
     else:
         output = output[0]
     output = (output * 255).astype('uint8')
     output = cv2.resize(output, origin_shape)
-    cv2.imwrite('output/generated' + str(imgid) + '.png', )
+    cv2.imwrite('output/generated' + str(imgid) + '.png', output)
 
 actor = ResNet(9, 18, 65) # action_bundle = 5, 65 = 5 * 13
 actor.load_state_dict(torch.load(args.actor))
